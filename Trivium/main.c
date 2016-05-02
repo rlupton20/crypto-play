a2f3a57c492ea39f4de083ae0a6240f030052bf4
Richard Lupton <richard.lupton@gmail.com>
2016-05-02 17:30:58 +0100

    Initial commit.

---
+++ Trivium/main.c
@@ -0,0 +1,10 @@
+/* This is a driver for the trivium cipher (to
+   see how it works, etc. */
+
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  printf("Trivium driver.\n");
+  return 0;
+}
